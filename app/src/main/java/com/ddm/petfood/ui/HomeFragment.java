37069b63ca99a4d79ec868dfe7c0f6e1e071201a
joaomarcuscardoso <joao.cardoso@tikiweb.com.br>
2023-10-28 19:18:52 -0300

    Criando estrutura RecycleView da Home

--- app/src/main/java/com/ddm/petfood/ui/HomeFragment.java
+++ app/src/main/java/com/ddm/petfood/ui/HomeFragment.java
@@ -1,65 +1,66 @@
 package com.ddm.petfood.ui;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ddm.petfood.R;
+import com.ddm.petfood.adapter.PetAdapter;
+import com.ddm.petfood.entity.Pet;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+    private Context context;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private PetAdapter petAdapter;
+    private List<Pet> pets;
+    private Handler handler = new Handler();
+    private PetAdapter adapter;
+    private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        context = getContext();
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+
+        pets = new ArrayList<>();
+        // drawable/baseline_person_outline_24"
+        for (int i = 0; i < 20; i++) {
+            Pet pet = null;
+            if (i % 2 == 0) {
+                pet = new Pet("Beer " + i, "Gato", new Date(), R.drawable.baseline_person_outline_24);
+                pet.setInfo("Gato muito fofo");
+            }
+            else {
+                pet = new Pet("Ted " + i, "Cachorro", new Date(), R.drawable.outline_pets_24);
+                pet.setInfo("Cachorro bricalhão");
+            }
+
+            pets.add(pet);
+        }
+
+        petAdapter = new PetAdapter(context, pets);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(petAdapter);
+
+       return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
     }
 }
