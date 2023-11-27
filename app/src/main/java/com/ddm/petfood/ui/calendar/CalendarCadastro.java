19d25d382eb89f3014844777cf75095e4e37a4df
Igor Meurer <Igorigormeurer@hotmail.com>
2023-11-26 21:29:01 -0300

    Feito o começo das telas de calendar

---
+++ app/src/main/java/com/ddm/petfood/ui/calendar/CalendarCadastro.java
@@ -0,0 +1,16 @@
+package com.ddm.petfood.ui.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ddm.petfood.R;
+
+public class CalendarCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_cadastro);
+    }
+}
